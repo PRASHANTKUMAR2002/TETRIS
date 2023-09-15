24ff0e1cba57bd31f8aaf06ab88ba03e1a5f0e0b
PRASHANTKUMAR2002 <prashantkumarg2001@gmail.com>
2023-09-15 21:46:37 +0530

    Initial commit

---
+++ src/containers/GameOver.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { StyleSheet, css } from 'aphrodite';
+
+
+import { Twitter, Facebook, Whatsapp } from 'react-social-sharing'
+import { getHighScore } from '../config/SaveScore';
+
+const styles = StyleSheet.create({
+    container: {
+        display: 'flex',
+        flex: 1,
+        flexDirection: 'column',
+        textAlign: 'center',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+});
+
+
+export default class GameOver extends Component {
+
+    render() {
+
+        return (
+            <div className={css(styles.container)}>
+                <h2>Game Over</h2>
+                <h3>{`You scored ${this.props.score}`}</h3>
+            </div>
+        );
+    }
+}
+
+
+
+
